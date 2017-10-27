d06061b0e8ab2e57de62a95ae50db3baa42c8a38
ator89 <ator89@outlook.com>
2017-10-27 14:43:51 -0600

    hola

--- Lab3_MelvinV_AngelT/src/lab3_melvinv_angelt/Lab3_MelvinV_AngelT.java
+++ Lab3_MelvinV_AngelT/src/lab3_melvinv_angelt/Lab3_MelvinV_AngelT.java
@@ -1,11 +1,60 @@
 
 package lab3_melvinv_angelt;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 
 public class Lab3_MelvinV_AngelT {
 
    
     public static void main(String[] args) {
+        
+        
+        
+        ArrayList lista=new ArrayList();
+        
+        String opcion="";
+        
+        while (!opcion.equals("e")){
+            opcion=JOptionPane.showInputDialog(""
+                    + "a - Log in\n"
+                    + "b - Registrarse in\n"
+                    + "e - Salir\n"
+                    
+                    + "");
+            
+            if (opcion.equals("a"))
+            {
+                int p;
+                p=Integer.parseInt(JOptionPane.showInputDialog("Ingrese opción: \n"+
+                        "1 - Socio"+
+                        "2 - Clientes"));
+                
+                if (p==1)
+                {
+                    JOptionPane.showInputDialog("Ingrese opción: \n"+
+                        "1 - Locales"+
+                        "2 - Personas");
+                }
+                
+                if (p==2)
+                {
+                    
+                }
+                
+                else{
+                    JOptionPane.showMessageDialog(null,"Ingrese la opción correcta");
+                }
+            }
+            
+            if (opcion.equals("b"))
+            {
+                
+            }
+            
+            
+        }
         
     }
     
